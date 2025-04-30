2aaff03172714f38d3bcb95c806a93decb442e50
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-04-30 11:59:09 -0300

    fazendo a pagina de edição

---
+++ src/pages/Perfil.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Perfil = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center" style={{ height: "100dvh" }}>
+          <div className="edição m-3 mt-0 colum"></div>
+      <div className="perfil "></div>
+      
+  
+    </div>
+  );
+};
+
+export default Perfil;
