8fb86d7ac13896fa36cfb5d869c0334ea11fa98d
Ana Chiaramonte <anasenai.lopes@gmail.com>
2025-05-06 12:02:06 -0300

    Terminando a pagina de cadastro

--- src/pages/Cartoes.jsx
+++ src/pages/Cartoes.jsx
@@ -1,22 +1,81 @@
-import React from 'react'
 
+import { Link } from "react-router";
+import HeaderLogin from "../components/HeaderLogin";
+import React, { useEffect, useState } from "react";
+import Perfil from "./Perfil";
 
 const Cartoes = () => {
+  const [usuario, setUsuario] = useState([]);
+  const [nomeEditado, setNomeEditado] = useState("");
+  const [emailEditado, setEmailEditado] = useState("");
+  const [senhaEditada, setSenhaEditada] = useState("");
+  const [dataEditada, setDataEditada] = useState("");
+  const [editar, setEditar] = useState("disabled");
+
+  useEffect(() => {
+    editar === "" && document.getElementById("nome").focus();
+  }, [editar]);
+
+  const disableInput = () => {
+    editar === "disabled" ? setEditar("") : setEditar("disabled");
+  };
+
+  useEffect(() => {
+    const salvaUsuario = localStorage.getItem("devlogin");
+    if (salvaUsuario) {
+      const user = JSON.parse(salvaUsuario);
+      setUsuario(user);
+      setNomeEditado(user.nome); // define valor inicial
+      setEmailEditado(user.email); // define valor inicial
+      setSenhaEditada(user.senha); // define valor inicial
+      setDataEditada(user.data); // define valor inicial
+    }
+  }, []);
+
+  const handleSave = () => {
+    const usuarioAtualizado = {
+      ...usuario,
+      nome: nomeEditado,
+      email: emailEditado,
+      senha: senhaEditada,
+      data: dataEditada,
+    };
+
+    setUsuario(usuarioAtualizado);
+    localStorage.setItem("devlogin", JSON.stringify(usuarioAtualizado));
+    setEditar("disabled");
+    alert("🎲Dados salvos com sucesso!😁");
+  };
+
+
   return (
-    <div
-
-   className='Cartões d-flex justify-content-center align-items-center'
-   
->
-<div className="d-flex">
-    
-    <div className="d-flex flex-column gap-2 m-3">
-        
+    <>
+      <HeaderLogin />
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100dvh" }}
+      >
+        <div className="d-flex">
+          <div className="d-flex flex-column gap-2 m-3">
+            <Link to={"/Perfil"} className="text-light text-decoration-none">
+              <button className="edicao">
+                <h5 className="m-0 text-light mt-1">Cadastro</h5>
+                <p className="textEdicao">Edite seus dados</p>
+              </button>
+            </Link>
+            <button className="edicao">
+              <h5 className="m-0 text-light mt-1">Cartões</h5>
+              <p className="textEditar">Ver seus cartões cadastrados</p>
+            </button>
+          </div>
+          <div className="cartoes"> 
       </div>
-  </div>
-</div>
-
+      </div>
+      </div>
+       
+   
+    </>
   );
 };
 
-export default Cartoes
+export default Cartoes;
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,47 +1,154 @@
-import React from "react";
-
 import Cartoes from "./Cartoes";
-import { Link } from "react-router-dom";
-
-
-
-
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import HeaderLogin from "../components/HeaderLogin";
 
 const Perfil = () => {
+  const [usuario, setUsuario] = useState([]);
+  const [nomeEditado, setNomeEditado] = useState("");
+  const [emailEditado, setEmailEditado] = useState("");
+  const [senhaEditada, setSenhaEditada] = useState("");
+  const [dataEditada, setDataEditada] = useState("");
+  const [editar, setEditar] = useState("disabled");
+
+  useEffect(() => {
+    editar === "" && document.getElementById("nome").focus();
+  }, [editar]);
+
+  const disableInput = () => {
+    editar === "disabled" ? setEditar("") : setEditar("disabled");
+  };
+
+  useEffect(() => {
+    const salvaUsuario = localStorage.getItem("devlogin");
+    if (salvaUsuario) {
+      const user = JSON.parse(salvaUsuario);
+      setUsuario(user);
+      setNomeEditado(user.nome); // define valor inicial
+      setEmailEditado(user.email); // define valor inicial
+      setSenhaEditada(user.senha); // define valor inicial
+      setDataEditada(user.data); // define valor inicial
+    }
+  }, []);
+
+  const handleSave = () => {
+    const usuarioAtualizado = {
+      ...usuario,
+      nome: nomeEditado,
+      email: emailEditado,
+      senha: senhaEditada,
+      data: dataEditada,
+    };
+
+    setUsuario(usuarioAtualizado);
+    localStorage.setItem("devlogin", JSON.stringify(usuarioAtualizado));
+    setEditar("disabled");
+    alert("🎲Dados salvos com sucesso!😁");
+  };
+
   return (
-    <div
-      className="d-flex justify-content-center align-items-center"
-      style={{ height: "100dvh" }}
-      
-    >
-      
-      <div className="d-flex">
-    
-        <div className="d-flex flex-column gap-2 m-3">
+    <>
+      <HeaderLogin />
 
-         <button  className="edicao"  ><h5 className="m-0 text-light mt-1">
-          
-        Cadastro</h5><p className="textEdicao">Edite seus dados</p>
-       
-     
-         </button>  
-        
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100dvh" }}
+      >
+        <div className="d-flex">
+          <div className="d-flex flex-column gap-2 m-3">
+            <button className="edicao">
+              <h5 className="m-0 text-light mt-1">Cadastro</h5>
+              <p className="textEdicao">Edite seus dados</p>
+            </button>
+
+            <Link to={"/cartoes"} className="text-light text-decoration-none">
+              <button className="edicao">
+                <h5 className="m-0 text-light mt-1">Cartões</h5>
+                <p className="textEditar">Ver seus cartões cadastrados</p>
+              </button>
+            </Link>
+          </div>
+          <div className="cadastro d-flex flex-column align-items-start p-5 gap-3">
+            <div className="d-flex justify-content-between w-100">
+              <div>
+                <h5 className="text-light">Cadastro</h5>
+                <p className="textEdicao">Nome cadastrado</p>
+              </div>
+              <button
+                className="btn btn-danger h-75"
+                onClick={() => disableInput()}
+              >
+                editar
+              </button>
+            </div>
             
-      
-         <button className="edicao"><h5 className="m-0 text-light mt-1"> 
-          <Link to={"/cartoes"} className="text-light text-decoration-none">
-            Cartões
-          </Link>
-          </h5><p className="textEditar">Ver seus cartões cadastrados</p></button>
+            <input
+              id="nome"
+              type="text"
+              placeholder="Edite seu nome"
+              className={`form-control w-100 ${editar}`}
+              disabled={editar}
+              value={nomeEditado}
+              onChange={(e) => setNomeEditado(e.target.value)}
+            />
+             <p className="text-light m-0">Email</p>
+                <input
+              id="email"
+              type="text"
+              placeholder="Edite seu email"
+              className={`form-control w-100 ${editar}`}
+              disabled={editar}
+              value={emailEditado}
+              onChange={(e) => setEmailEditado(e.target.value)}
+            />
+             <div class="col-auto">
+             <p className="text-light">Senha</p>
+              <input
+              class="form-control"
+              id="senha"
+              type="password"
+              placeholder="Edite sua senha"
+              className={`form-control w-100 aria-describedby="passwordHelpInline col-auto col-form-label" ${editar}`}
+              disabled={editar}
+              value={senhaEditada}
+              onChange={(e) => setSenhaEditada(e.target.value)}
+              
+            />
+
+</div>
+  
+<div  >
+
+        <p className="text-light">Data Nascimento</p>
+        <select class="form-select" aria-label="Default select example">
+              <input
+             
+              id="data"
+              type="number"
+              placeholder="Mês"
+            
+              disabled={editar}
+              value={dataEditada}
+              onChange={(e) => setDataEditada(e.target.value)}
+              
+            />
+
+  <option date></option>
+  
+</select>
+</div>
+            <button
+              className="btn btn-success shadow "
+              disabled={editar}
+              onClick={() => handleSave()}
+            >
+              Salvar
+            </button>
+          </div>
         </div>
-        <div className="perfil "></div>
       </div>
-    </div>
-    
-
+    </>
   );
-
 };
 
 export default Perfil;
